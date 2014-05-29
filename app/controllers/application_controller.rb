cfff8e174819d7c9969d6199f23d6944a3d31979
Aaron Groch <ar.groch@gmail.com>
2014-05-28 20:45:03 -0400

    more styling changes: add a bg image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  bg = ['marta-img1.jpg','marta-img2.jpg','marta-img3.jpg','marta-img4.jpg']
+  i = rand(0..(bg.length-1))
+  @selectedBg = bg[i]
+
+  puts "*********"
+  puts @selectedBg
+  puts "*********"
+
 end
